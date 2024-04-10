4abc9d7681d4ca4f764d166605982ed0586fd208
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2024-04-09 23:31:12 -0300

    fix: resource scheduling dor retries is enabled on Application class

--- src/main/java/br/com/itau/banktransfer/BankTransferApplication.java
+++ src/main/java/br/com/itau/banktransfer/BankTransferApplication.java
@@ -3,16 +3,17 @@ package br.com.itau.banktransfer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 public class BankTransferApplication {
 
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(BankTransferApplication.class, args);
 	}
-
 }
