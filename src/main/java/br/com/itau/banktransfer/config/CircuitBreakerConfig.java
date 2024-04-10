77908864d7abc320e4da841261d43e6a91b26333
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2024-04-09 23:22:18 -0300

    feat: add configuration for CircuitBreaker print log when change states

---
+++ src/main/java/br/com/itau/banktransfer/config/CircuitBreakerConfig.java
@@ -0,0 +1,33 @@
+package br.com.itau.banktransfer.config;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.core.registry.EntryAddedEvent;
+import io.github.resilience4j.core.registry.EntryRemovedEvent;
+import io.github.resilience4j.core.registry.EntryReplacedEvent;
+import io.github.resilience4j.core.registry.RegistryEventConsumer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class CircuitBreakerConfig {
+
+    @Bean
+    public RegistryEventConsumer<CircuitBreaker> getLog() {
+        return new RegistryEventConsumer<CircuitBreaker>() {
+            @Override
+            public void onEntryAddedEvent(EntryAddedEvent<CircuitBreaker> entryAddedEvent) {
+                entryAddedEvent.getAddedEntry()
+                        .getEventPublisher()
+                        .onStateTransition(event -> log.info("[CircuitBreakerConfig] - {}", event));
+            }
+
+            @Override
+            public void onEntryRemovedEvent(EntryRemovedEvent<CircuitBreaker> entryRemoveEvent) {}
+
+            @Override
+            public void onEntryReplacedEvent(EntryReplacedEvent<CircuitBreaker> entryReplacedEvent) {}
+        };
+    }
+}
